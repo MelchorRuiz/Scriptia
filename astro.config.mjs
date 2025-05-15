616c08f477854a2877b042b452e2dd3c2a9bdd58
MelchorRuiz <ruizgonzalezmelchor3@gmail.com>
2025-05-15 10:48:40 +0200

    Add Clerk integration and Flowbite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-
 import preact from '@astrojs/preact';
-
 import node from '@astrojs/node';
+import clerk from "@clerk/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  adapter: node({
-    mode: 'standalone'
-  }),
+  integrations: [ preact(), clerk() ],
+  vite: { plugins: [tailwindcss()], },
+  adapter: node({ mode: 'standalone' }),
   output: 'server',
 });
