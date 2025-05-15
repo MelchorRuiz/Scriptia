69d98380449c35e0821ab07f341c0e678330690f
MelchorRuiz <ruizgonzalezmelchor3@gmail.com>
2025-05-15 15:55:55 +0200

    Refactor sign-in page layout and enhance terminal emulator functionality

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,6 @@ export const onRequest = clerkMiddleware((auth, context) => {
     const { userId, redirectToSignIn } = auth() 
 
     if (isProtectedRoute(context.request) && !userId) {
-        return redirectToSignIn()
+        return context.redirect('/sign-in');
     }
 });
