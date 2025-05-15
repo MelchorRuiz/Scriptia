a32fcd670720bb15c98f53d29ca59ccb6ab27808
MelchorRuiz <ruizgonzalezmelchor3@gmail.com>
2025-05-15 15:55:50 +0200

    Add Clerk localization and theme support, include Mad Hacker font

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,20 @@ import preact from '@astrojs/preact';
 import node from '@astrojs/node';
 import clerk from "@clerk/astro";
 
+import { esMX } from '@clerk/localizations'
+import { dark } from '@clerk/themes'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [ preact(), clerk() ],
+  integrations: [ 
+    preact(), 
+    clerk({
+      localization: esMX,
+      appearance: {
+        baseTheme: dark,
+      },
+    }),
+  ],
   vite: { plugins: [tailwindcss()], },
   adapter: node({ mode: 'standalone' }),
   output: 'server',
