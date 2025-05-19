6f826a97ae9780accbd91507c8481bb20a290abf
MelchorRuiz <ruizgonzalezmelchor3@gmail.com>
2025-05-19 11:48:04 +0200

    feat: update route matcher to include '/explore' and '/post/:postId' for protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,17 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
 
-const isProtectedRoute = createRouteMatcher(["/", "/profile", "/saved", "/likes", "/new-post"]);
+const isProtectedRoute = createRouteMatcher([
+    "/", 
+    "/explore",
+    "/profile", 
+    "/saved", 
+    "/likes", 
+    "/new-post",
+    "/post/:postId",
+]);
 
 export const onRequest = clerkMiddleware((auth, context) => {
-    const { userId, redirectToSignIn } = auth() 
+    const { userId } = auth() 
 
     if (isProtectedRoute(context.request) && !userId) {
         return context.redirect('/sign-in');
