dff79fd1f7cee60f0fe3b5b06bee85109f16463b
MelchorRuiz <ruizgonzalezmelchor3@gmail.com>
2025-05-19 17:15:34 +0200

    feat: add warning message about code execution environment in ShowCode component

--- src/components/ShowCode.tsx
+++ src/components/ShowCode.tsx
@@ -53,18 +53,18 @@ export default function ShowCode({ language, dependecies, code }: ShowCodeProps)
     }, [taskId]);
 
     return (
-        <div className="overflow-x-auto">
+        <div>
             <div className='flex flex-col gap-2 mb-3'>
                 {dependecies.map((dependency, index) => (
-                    <div key={index} className="flex flex-col lg:flex-row w-full">
-                        <div className='bg-gray-700 flex items-center justify-center px-4 text-white text-nowrap lg:rounded-l-lg h-8 lg:h-auto text-sm'>
+                    <div key={index} className="flex w-full rounded-lg">
+                        <div className='bg-gray-700 flex items-center justify-center px-4 text-white text-nowrap text-sm rounded-l-lg'>
                             {language === 'python' ? 'pip install' :
                                 language === 'nodejs' ? 'npm install' :
                                     language === 'ubuntu' ? 'apt-get install' :
                                         language === 'alpine' ? 'apk add' : ''
                             }
                         </div>
-                        <p className="block p-2.5 text-sm gray-100 border bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus-visible:outline-none flex-1">
+                        <p className="block p-2.5 text-sm gray-100 border bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus-visible:outline-none flex-1 rounded-r-lg">
                             {dependency}
                         </p>
                     </div>
@@ -82,6 +82,21 @@ export default function ShowCode({ language, dependecies, code }: ShowCodeProps)
                     editable={false}
                     basicSetup={{ searchKeymap: false, foldGutter: false }}
                 />
+            </div>
+            <div className="mb-3">
+                <div className='flex items-center gap-2 p-4 rounded-lg opacity-80 ring-1 ring-neutral-200'>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-12 text-neutral-900 fill-yellow-700">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+                        </svg>
+                    </div>
+                    <div>
+                        <p className='text-yellow-700 font-bold'>Advertencia</p>
+                        <p className='text-neutral-200 text-sm'>
+                            El código se ejecuta en un contenedor Docker aislado. No tiene acceso a recursos del sistema como namespaces, dispositivos o archivos externos, ni consola interactiva (no se puede usar input() en Python). Sin embargo, sí cuenta con acceso a internet. Ten esto en cuenta al ejecutar tu código.
+                        </p>
+                    </div>
+                </div>
             </div>
             <div className='mb-3'>
                 <div className="flex flex-col lg:flex-row w-full">
